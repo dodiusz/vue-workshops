abbfa5f3f06d61f07d7fbb7f1852d0f5d2e6a785
Przemysław Kadej <pkadej@bluesoft.net.pl>
2019-03-16 10:16:19 +0100

    [#1.2] Add Add pages: Home, Vote, Score, Settings

---
+++ workshops/src/views/Score.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Score'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ workshops/src/views/Vote.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is vite
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Vote'
+}
+</script>
+
+<style scoped>
+
+</style>
